599dac38ae37f465e894951e92384869fb282b54
Deep Soni <deepso@amdocs.com>
2017-12-15 12:08:04 +0530

    Commenting Rest Call

--- src/components/studentTable.jsx
+++ src/components/studentTable.jsx
@@ -12,18 +12,17 @@ class StudentTable extends React.Component {
     return (<div>
       <table className='stuTable' cellSpacing='4' cellPadding='5' align='center'>
         <tbody>
-          {console.log('Data: '+JSON.stringify(this.state.data))}
-
+          {this.state.data.map((person, i) => <TableRow key={i} data={person}/>)}
         </tbody>
       </table>
     </div>);
   }
 
-  componentDidMount() {
+  /*componentDidMount() {
       fetch('http://localhost:8087/react/service/getTableData/').then(result => {
         this.setState({data: result});
       });
-    }
+    }*/
 }
 
 export default StudentTable;
